1356d996cac8efbfe935df286ecd007de5dbd6bb
ashleyjcast <ashleyj.castaneda@gmail.com>
2024-09-22 18:17:00 -0400

    complete

---
+++ EmployeeSQL/QuickDBD-Employee_Schema.sql
@@ -0,0 +1,136 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/nbqPMW
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "Departments" (
+    "dept_no" varchar(4)   NOT NULL,
+    "dept_name" varchar(20)   NOT NULL,
+    CONSTRAINT "pk_Departments" PRIMARY KEY (
+        "dept_no"
+     )
+);
+
+CREATE TABLE "Dept_emp" (
+    "emp_no" int   NOT NULL,
+    "dept_no" varchar(4)   NOT NULL,
+    CONSTRAINT "pk_Dept_emp" PRIMARY KEY (
+        "emp_no","dept_no"
+     )
+);
+
+CREATE TABLE "Dept_manager" (
+    "dept_no" varchar(4)   NOT NULL,
+    "emp_no" int   NOT NULL,
+    CONSTRAINT "pk_Dept_manager" PRIMARY KEY (
+        "dept_no","emp_no"
+     )
+);
+
+CREATE TABLE "Employees" (
+    "emp_no" int   NOT NULL,
+    "emp_title_id" varchar(5)   NOT NULL,
+    "birth_date" DATE   NOT NULL,
+    "first_name" varchar(20)   NOT NULL,
+    "last_name" varchar(20)   NOT NULL,
+    "sex" varchar(1)   NOT NULL,
+    "hire_date" DATE   NOT NULL,
+    CONSTRAINT "pk_Employees" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "Salaries" (
+    "emp_no" int   NOT NULL,
+    "salary" int   NOT NULL,
+    CONSTRAINT "pk_Salaries" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "Titles" (
+    "title_id" varchar(5)   NOT NULL,
+    "title" varchar(20)   NOT NULL,
+    CONSTRAINT "pk_Titles" PRIMARY KEY (
+        "title_id"
+     )
+);
+
+ALTER TABLE "Dept_emp" ADD CONSTRAINT "fk_Dept_emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employees" ("emp_no");
+
+ALTER TABLE "Dept_emp" ADD CONSTRAINT "fk_Dept_emp_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "Departments" ("dept_no");
+
+ALTER TABLE "Dept_manager" ADD CONSTRAINT "fk_Dept_manager_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "Departments" ("dept_no");
+
+ALTER TABLE "Dept_manager" ADD CONSTRAINT "fk_Dept_manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employees" ("emp_no");
+
+ALTER TABLE "Employees" ADD CONSTRAINT "fk_Employees_emp_title_id" FOREIGN KEY("emp_title_id")
+REFERENCES "Titles" ("title_id");
+
+ALTER TABLE "Salaries" ADD CONSTRAINT "fk_Salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employees" ("emp_no");
+
+SELECT table_name 
+FROM information_schema.tables 
+WHERE table_schema = 'public';
+
+SELECT *
+FROM "Departments";
+
+SELECT *
+FROM "Dept_emp";
+
+SELECT *
+FROM "Dept_manager";
+
+SELECT * 
+FROM "Employees";
+
+SELECT * 
+FROM "Salaries";
+
+SELECT *
+FROM "Titles";
+
+SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
+FROM "Employees" as e
+JOIN "Salaries" as s ON e.emp_no = s.emp_no;
+
+SELECT first_name, last_name, hire_date
+From "Employees" 
+WHERE EXTRACT(YEAR FROM hire_date) = 1986;
+
+SELECT dm.dept_no, d.dept_name, dm.emp_no, e.last_name, e.first_name 
+FROM "Dept_manager" as dm
+JOIN "Employees" as e ON dm.emp_no = e.emp_no
+JOIN "Departments" as d ON dm.dept_no = d.dept_no;
+
+SELECT de.dept_no, de.emp_no, e.last_name, e.first_name, d.dept_name
+FROM "Dept_emp" as de
+JOIN "Employees" as e ON de.emp_no = e.emp_no
+JOIN "Departments" as d ON de.dept_no = d.dept_no;
+
+SELECT first_name, last_name, sex
+FROM "Employees"
+WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
+
+SELECT e.emp_no, e.last_name, e.first_name
+FROM "Employees" as e
+JOIN "Dept_emp" as de ON e.emp_no = de.emp_no
+JOIN "Departments" as d ON de.dept_no = d.dept_no
+WHERE d.dept_name = 'Sales';
+
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM "Employees" as e 
+JOIN "Dept_emp" as de on e.emp_no = de.emp_no
+JOIN "Departments" as d ON de.dept_no = d.dept_no
+WHERE d.dept_name = 'Sales' OR d.dept_name = 'Development';
+
+SELECT last_name, count(*) As frequency
+FROM "Employees"
+GROUP BY last_name
+ORDER BY frequency DESC;
